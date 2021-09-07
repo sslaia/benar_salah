023411fcbe52dd812132c11bb025b698e54421c1
Sirus Laia <laia.sirus@gmail.com>
2021-09-07 10:44:42 +0100

    Titik tolak aplikasi Benar Salah

---
+++ README.md
@@ -0,0 +1,24 @@
+# benar_salah
+
+Aplikasi ini merupakan bahan untuk belajar membuat kuis, berdasarkan aplikasi yang telah dibuat
+sebelumnya **Burung Nias**
+
+## Bagaimana mulai?
+
+1. Ciptakan aplikasi baru Flutter di dalam **Android Studio**: File > New > New Flutter project
+2. Beri nama aplikasi **benar_salah**
+3. Setelah selesai, buka berkas **main.dart** dan kosongkan (hapus semua kode yang telah tersedia).
+4. Buka berkas main.dart dari aplikasi **Burung Nias**
+5. Kopi semua isinya, lalu sisipkan di dalam berkas main.dart aplikasi **Benar Salah**
+6. Sesuaikan judul (ganti Burung Nias ke Benar Salah)
+7. Selesai
+
+## Bantuan
+
+Semua panduan untuk belajar bersama Flutter ada di [Belajar Bersama Flutter](https://github.com/sslaia/belajar_flutter)
+
+Selebihnya tersedia dokumentasi, panduan, tutorial di website Flutter:
+
+- [Lab: Write your first Flutter app](https://flutter.dev/docs/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://flutter.dev/docs/cookbook)
+- [Online documentation](https://flutter.dev/docs)
---
+++ lib/main.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Benar Salah',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Benar Salah'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  List<String> _burungNias = ["Beo", "Buru'u", "Magio", "Nagoyomanase", "Towi-towi", "Nazese", "Gogowaya", "Miti-miti", "Siliŵi", "Boroa"];
+  String _burungPilihan = "Beo";
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+      if (_counter < _burungNias.length) {
+        _burungPilihan = _burungNias[_counter];
+      }
+      else {
+        _counter = 0;
+        _burungPilihan = _burungNias[_counter];
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+
+      if (_counter >= 0) {
+        _burungPilihan = _burungNias[_counter];
+      }
+      else {
+        _counter = _burungNias.length - 1;
+        _burungPilihan = _burungNias[_counter];
+      }
+    });
+  }
+
+  void _sembarangBurung() {
+    setState(() {
+      _counter = Random().nextInt(9) + 1;
+      _burungPilihan = _burungNias[_counter];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Burung pilihan Anda: ',
+            ),
+            Text(
+              '$_burungPilihan',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: _decrementCounter, child: Text('<'),),
+                SizedBox(width: 50,),
+                ElevatedButton(onPressed: _incrementCounter, child: Text('>'),),
+              ],
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _sembarangBurung,
+        tooltip: 'Increment',
+        child: Icon(Icons.shuffle),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
